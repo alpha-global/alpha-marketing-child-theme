4e3e5ebff21ed6348f9f8a2d68682488b6956d02
Welton Demetrio <welton.demetrio@alpha.org>
2021-07-22 20:26:14 -0300

    Added the donation form to the donate-page on the child-theme. This runs on the **donate** page only.

--- functions.php
+++ functions.php
@@ -15,5 +15,14 @@ function alpha_child_enqueue_styles() {
 	$theme        = wp_get_theme();
 	wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', array(), $theme->parent()->get( 'Version' ) );
 	wp_enqueue_style( 'alpha-child-style', get_stylesheet_uri(), array( $parenthandle ), $theme->get( 'Version' ) );
+	wp_enqueue_style( 'donate', get_stylesheet_directory_uri() . '/css/donation-form.css', false, '', 'all');
+
 }
 add_action( 'wp_enqueue_scripts', 'alpha_child_enqueue_styles' );
+
+add_action(
+	'init', function() {
+		wp_register_script( 'donate', get_stylesheet_directory_uri() . '/js/donate.js', array( 'jquery' ), '', false );
+		wp_enqueue_script( 'donate' );
+	}
+);
